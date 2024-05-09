736fbe63386e73b02db89855f9e83402c8f49da7
WittlyEli <162608185+EliRod61@users.noreply.github.com>
2024-05-09 02:08:53 -0400

    Created item behavior

--- HeroBornUnityGame/Assets/Scripts/PlayerBehavior.cs
+++ HeroBornUnityGame/Assets/Scripts/PlayerBehavior.cs
@@ -28,4 +28,17 @@ public class PlayerBehavior : MonoBehaviour
         this.transform.Rotate(Vector3.up * _hInput * Time.deltaTime);
         */
     }
+    void FixedUpdate()
+    {
+        // 2
+        Vector3 rotation = Vector3.up * _hInput;
+        // 3
+        Quaternion angleRot = Quaternion.Euler(rotation *
+        Time.fixedDeltaTime);
+        // 4
+        _rb.MovePosition(this.transform.position +
+        this.transform.forward * _vInput * Time.fixedDeltaTime);
+        // 5
+        _rb.MoveRotation(_rb.rotation * angleRot);
+    }
 }
