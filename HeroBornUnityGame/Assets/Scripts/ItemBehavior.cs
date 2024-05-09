e98a87ed81cff0a9ccbc7bf82dbf0bc14a925a98
WittlyEli <162608185+EliRod61@users.noreply.github.com>
2024-05-09 02:59:30 -0400

    More prefabs and finished HeroBorn part 2

--- HeroBornUnityGame/Assets/Scripts/ItemBehavior.cs
+++ HeroBornUnityGame/Assets/Scripts/ItemBehavior.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class ItemBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnCollisionEnter(Collision collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collision.gameObject.name == "Player")
+        {
+            Destroy(this.transform.gameObject);
+            
+            Debug.Log("Item collected!");
+        }
     }
 }
