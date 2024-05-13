c7129cd03fe7d23fdad4329756497a381fb62ecb
WittlyEli <162608185+EliRod61@users.noreply.github.com>
2024-05-13 15:28:04 -0400

    Created bullet prefab and added shooting mechanic to player

--- HeroBornUnityGame/Assets/Scripts/PlayerBehavior.cs
+++ HeroBornUnityGame/Assets/Scripts/PlayerBehavior.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
+    private Rigidbody _rb;
+
     //Variables for moving and rotating
     public float MoveSpeed = 10f;
     public float RotateSpeed = 75f;
     private float _vInput;
     private float _hInput;
-
-    private Rigidbody _rb;
 
     //Variables for Jumping
     public float JumpVelocity = 5f;
@@ -19,6 +19,11 @@ public class PlayerBehavior : MonoBehaviour
     public float DistanceToGround = 0.1f;
     public LayerMask GroundLayer;
     private CapsuleCollider _col;
+
+    //Shooting
+    public GameObject Bullet;
+    public float BulletSpeed = 100f;
+    private bool _isShooting;
 
     // Start is called before the first frame update
     void Start()
@@ -38,6 +43,9 @@ public class PlayerBehavior : MonoBehaviour
         //Moves and rotates with player input
         _vInput = Input.GetAxis("Vertical") * MoveSpeed;
         _hInput = Input.GetAxis("Horizontal") * RotateSpeed;
+
+        //Shoot bullets when space bar is hit
+        _isShooting |= Input.GetMouseButton(0);
 
         /*
         this.transform.Translate(Vector3.forward * _vInput * Time.deltaTime);
@@ -59,8 +67,21 @@ public class PlayerBehavior : MonoBehaviour
         }
 
         _isJumping = false;
+
+        //Shooting: bullets forced to same position and rotation/direction when shooting
+        if (_isShooting)
+        {
+            GameObject newBullet = Instantiate(Bullet, this.transform.position + new Vector3(0, 0, 1), this.transform.rotation);
+
+            Rigidbody BulletRB = newBullet.GetComponent<Rigidbody>();
+
+            BulletRB.velocity = this.transform.forward * BulletSpeed;
+        }
+
+        _isShooting = false;
+
     }
-    private bool IsGrounded()
+        private bool IsGrounded()
     {
         Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, _col.bounds.min.y, _col.bounds.center.z);
 
